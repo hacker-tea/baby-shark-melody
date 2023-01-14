84af18a31d81326ea988e4b4c62bf6cf6994a0a7
hacker-tea <121435491+hacker-tea@users.noreply.github.com>
2023-01-14 15:35:55 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,13 @@
+basic.forever(function () {
+    music.playTone(262, music.beat(BeatFraction.Half))
+    music.playTone(294, music.beat(BeatFraction.Half))
+    music.playTone(349, music.beat(BeatFraction.Half))
+    music.playTone(349, music.beat(BeatFraction.Half))
+    music.playTone(349, music.beat(BeatFraction.Half))
+    music.playTone(349, music.beat(BeatFraction.Quarter))
+    music.playTone(349, music.beat(BeatFraction.Quarter))
+    music.playTone(349, music.beat(BeatFraction.Half))
+    music.playTone(349, music.beat(BeatFraction.Quarter))
+    music.playTone(349, music.beat(BeatFraction.Half))
+    basic.pause(100)
+})
